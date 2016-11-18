55675f35cc5c4c360dc0588e171fc3692f918638
Nikolas Metcalfe <nikolasmetcalfe@Nikolass-MacBook-Pro.local>
2016-11-18 15:57:16 -0500

    Milestone 2 Form

--- tableGen/routes/index.js
+++ tableGen/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 require('../db.js');
 
 var mongoose = require('mongoose');
-var ImagePost = mongoose.model('User');
-var Image = mongoose.model('Patch');
+var User = mongoose.model('User');
+var Patch = mongoose.model('Patch');
 
 router.get('/', function(req, res, next)
 {
@@ -25,12 +25,77 @@ router.get('/patches', function(req, res, next)
 });
 
 router.post('/generate', function(req, res, next)
-{
-	res.redirect('generate');
+{	
+	vol = [];
+	
+	var vol0 = (req.body.vol0);
+	var vol1 = (req.body.vol1);
+	var vol2 = (req.body.vol2);
+	var vol3 = (req.body.vol3);
+	var vol4 = (req.body.vol4);
+	var vol5 = (req.body.vol5);
+	var vol6 = (req.body.vol6);
+	var vol7 = (req.body.vol7);
+	var vol8 = (req.body.vol8);
+	var vol9 = (req.body.vol9);
+	var vol10 = (req.body.vol10);
+	var vol11 = (req.body.vol11);
+	var vol12 = (req.body.vol12);
+	var vol13 = (req.body.vol13);
+	var vol14 = (req.body.vol14);
+	var vol15 = (req.body.vol15);
+	
+	vol.push(vol0);
+	vol.push(vol1);
+	vol.push(vol2);
+	vol.push(vol3);
+	vol.push(vol4);
+	vol.push(vol5);
+	vol.push(vol6);
+	vol.push(vol7);
+	vol.push(vol8);
+	vol.push(vol9);
+	vol.push(vol10);
+	vol.push(vol11);
+	vol.push(vol12);
+	vol.push(vol13);
+	vol.push(vol14);
+	vol.push(vol15);
+	
+	var temp1 = '';
+	var temp2 = '';
+	
+	for (var i = 0; i < vol.length; i++)
+	{
+		if (vol[i].length === 1 || vol[i].length === 0)
+		{
+			temp1 = toHex('');
+			temp2 = toHex('');
+			vol[i] = temp1 + temp2;
+		}
+		else
+		{
+			if (checkInput(vol[i][0]))
+				temp1 = vol[i][0];
+			else
+				temp1 = toHex('');
+			if (checkInput(vol[i][1]))
+				temp2 = vol[i][1];
+			else
+				temp2 = toHex('');
+			vol[i] = temp1 + temp2;
+		}
+	}
 	
 	new Patch
 	({
-		volume: 
+		volume: vol,
+		transpose : [], 
+		commandOne : [], 
+		commandOneParams : [], 
+		commandTwo : [], 
+		commandTwoParams : [],
+		name: "empty"
 	}).save(function(err, Patch, count)
 	{
 		res.redirect('/patches');
@@ -107,6 +172,18 @@ function checkInput(a)
 	else if (a === "E")
 		return true;
 	else if (a === "F")
+		return true;
+	else if (a === "a")
+		return true;
+	else if (a === "b")
+		return true;
+	else if (a === "c")
+		return true;
+	else if (a === "d")
+		return true;
+	else if (a === "e")
+		return true;
+	else if (a === "f")
 		return true;
 	else
 		return false;	
