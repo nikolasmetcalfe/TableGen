fc68e7ea5a79ff27e608353f3c937990897b3f0c
Nikolas Metcalfe <nikolasmetcalfe@Nikolass-MacBook-Pro.local>
2016-12-06 01:48:37 -0500

    Sidebar

---
+++ tableGen/public/javascripts/domWork.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event)
+{
+  var button = document.getElementById("informationButton");
+  button.addEventListener('click', toggleInfo);
+});
+
+function toggleInfo()
+{
+	var div = document.getElementById("hidden");
+	if (div == undefined)
+	{
+		div = document.getElementById("visible");
+	}
+	
+	if (div.id == 'hidden')
+	{
+		div.id = "visible";
+	}
+	else
+		div.id = "hidden";
+}
